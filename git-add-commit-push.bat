f58b0b0a111550f1b2a290a70402d4420d131272
Shynur <one.last.kiss@outlook.com>
2023-02-12 19:00:09 +0800

    not 1st commit

--- git-add-commit-push.bat
+++ git-add-commit-push.bat
@@ -1,5 +1,5 @@
-git config --global user.name 'Shynur'
-git config --global user.email 'one.last.kiss@outlook.com'
+git config --global user.name "Shynur"
+git config --global user.email "one.last.kiss@outlook.com"
 git add .
 git commit -m "not 1st commit"
 git push -u origin main
