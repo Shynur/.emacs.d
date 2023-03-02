2deb02c318da48fa5596306dc6eeab32e661ba46
Shynur <one.last.kiss@outlook.com>
2023-03-02 23:47:11 +0800

    not 1st commit

--- git-add-commit-push.bat
+++ git-add-commit-push.bat
@@ -5,9 +5,4 @@ git add .
 git commit -m "not 1st commit"
 git branch -M main
 
-git config --global http.proxy http://127.0.0.1:1080
-git config --global --unset http.proxy
-git config --global https.proxy http://127.0.0.1:1080
-git config --global --unset https.proxy
-
 git push -u origin main
