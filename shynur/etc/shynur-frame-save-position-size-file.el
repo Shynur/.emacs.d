fe21c4c3165be024610f5f389140e0cee2cc754d
Shynur <one.last.kiss@outlook.com>
2023-03-12 17:09:20 +0800

    not 1st commit

--- shynur/etc/shynur-frame-save-position-size-file.el
+++
@@ -1,4 +0,0 @@
-(add-to-list 'initial-frame-alist '(left . 513))
-(add-to-list 'initial-frame-alist '(top . 411))
-(add-to-list 'initial-frame-alist '(width . 99))
-(add-to-list 'initial-frame-alist '(height . 23))
